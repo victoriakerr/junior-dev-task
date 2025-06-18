fcba2eb6397b2e2115db6c4b6b7d87be33bfe989
Victoria Kerr <vickykerr20@gmail.com>
2025-06-18 13:34:17 +0200

    Initial commit Express API + frontend for Junior Dev task

---
+++ api/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.post("/", (req, res) => {
+    const { data } = req.body;
+
+    if (typeof data !== "string") {
+        return res.status(400).json({ error: "Invalid input. 'data' must be a string."});
+    }
+
+    const sortedChars = data.split("").sort();
+    return res.json({ word: sortedChars });
+});
+
+module.exports = app;
